92d03cfabe37fe870850f7b8130cdd623c95a3fe
AlamGarry <alqifariq@gmail.com>
2024-07-24 10:51:52 +0700

    list exercise

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, FlatList } from 'react-native';
 import ColorBox from './components/ColorBox';
+
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
 
 export default function App() {
   return (
@@ -9,10 +28,14 @@ export default function App() {
           Here are some boxes of different color
         </Text>
 
-        <ColorBox colorName={'Cyan'} hexCode={'#2aa198'} />
-        <ColorBox colorName={'Blue'} hexCode={'#268bd2'} />
-        <ColorBox colorName={'Magenta'} hexCode={'#d33682'} />
-        <ColorBox colorName={'Orange'} hexCode={'#cb4b16'} />
+        <FlatList
+          style={{ padding: 20 }}
+          data={COLORS}
+          keyExtractor={(item) => item.colorName}
+          renderItem={({ item }) => (
+            <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+          )}
+        />
       </View>
     </SafeAreaView>
   );
@@ -21,7 +44,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
-    paddingHorizontal: 10,
+    paddingLeft: 10,
     backgroundColor: 'fff',
   },
   heading: {
--- components/ColorBox.js
+++ components/ColorBox.js
@@ -5,9 +5,15 @@ const ColorBox = ({ colorName, hexCode }) => {
   const boxColor = {
     backgroundColor: hexCode,
   };
+  const textStyle = {
+    color:
+      parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
+  };
   return (
     <View style={[styles.box, boxColor]}>
-      <Text style={styles.boxText}>
+      <Text style={[styles.text, textStyle]}>
         {colorName}: {hexCode}
       </Text>
     </View>
